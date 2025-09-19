05a1aef0ce3d8d246b2597acd2a2f445fccf2512
vm-mac <vm-mac@WIN-GQ2GLJ2D1D2>
2025-09-19 06:27:51 -0300

    Add project files.

---
+++ RevitAddIn/Commands/StartupCommand.cs
@@ -0,0 +1,37 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.UI;
+using Nice3point.Revit.Toolkit.External;
+
+namespace RevitAddIn.Commands
+{
+    /// <summary>
+    ///     External command entry point
+    /// </summary>
+    [UsedImplicitly]
+    [Transaction(TransactionMode.Manual)]
+    public class StartupCommand : ExternalCommand
+    {
+        public override void Execute()
+        {
+            TaskDialog.Show(Document.Title, "Revit Add-In is started!");
+
+            var titleBlockId = ElementId.InvalidElementId;
+
+            try
+            {
+                var sheet = ViewSheet.Create(Document, titleBlockId);
+            }
+            catch (Autodesk.Revit.Exceptions.ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Sheet could not be made\n");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+           
+
+        }
+    }
+}
---
+++ RevitAddIn/Forms/Custom.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAddIn.Forms
+{
+    public static class Custom
+    {
+    }
+
+    public class FormResult
+    {
+        public string test { get; set; }
+        //public FormResult()
+    }
+}
