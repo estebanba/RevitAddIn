59833754e82e90d1b2dc692a5947d8db7b188385
estebanba <esteban.ba@gmail.com>
2025-09-21 04:47:52 -0300

    add Globals class

--- Application.cs
+++ Application.cs
@@ -2,6 +2,9 @@
 using System.Reflection;
 // Autodesk
 using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Events;
+using RevitAddIn.General;
+
 // RevitAddIn
 using ribUtil = RevitAddIn.Utilities.Ribbon_Utils;
 
@@ -11,32 +14,77 @@ namespace RevitAddIn
     // Implementing the interface for applications
     public class Application: IExternalApplication
     {
+        // Make a private uiCtlApp
+        private static UIControlledApplication _uiCtlApp;
+
         // This will run on Startup
         public Result OnStartup(UIControlledApplication uiCtlApp)
         {
-            // Collect the controlled application
-            var ctlApp = uiCtlApp.ControlledApplication;
-            var assembly = Assembly.GetExecutingAssembly();
-            var assemblyPath = assembly.Location;
+            #region Globals registration
+            // Store the uiCtlApp in the private variable
+            _uiCtlApp = uiCtlApp;
 
-            string tabName = "RevitAddIn";
+            try
+            {
+                _uiCtlApp.Idling += RegisterUiApp;
+            }
+            catch
+            {
+                Globals.UiApp = null;
+                Globals.UsernameRevit = null;
+            }
+
+            // Globals
+            Globals.RegisterProperties(uiCtlApp);
+
+            // Collect the controlled application -> NOW IN GLOBALS
+            //var ctlApp = uiCtlApp.ControlledApplication;
+            //var assembly = Assembly.GetExecutingAssembly();
+            //var assemblyPath = assembly.Location;
+            //string tabName = "RevitAddIn";
+
+            #endregion
+
+            #region Ribbon setup
 
             // Add a new ribbon tab
-            ribUtil.AddRibbonTab(uiCtlApp, tabName);
+            ribUtil.AddRibbonTab(uiCtlApp, Globals.AddinName);
 
             // Add panels to the tab
-            var panelGeneral = ribUtil.AddRibbonPanelToTab(uiCtlApp, tabName, "General");
+            var panelGeneral = ribUtil.AddRibbonPanelToTab(uiCtlApp, Globals.AddinName, "General");
 
             // Add buttons to the panel
-            var buttonTest = ribUtil.AddPushButtonToPanel(panelGeneral, "Testing", "RevitAddIn.Commands.Cmds_General.Cmd_Test", "_testing", assemblyPath);
+            var buttonTest = ribUtil.AddPushButtonToPanel(panelGeneral, "Testing", "RevitAddIn.Commands.Cmds_General.Cmd_Test", "_testing", Globals.AssemblyPath);
 
+            #endregion
+
+            // Final return
             return Result.Succeeded;
         }
+
+        #region On shutdown method
 
         // This will run on Shutdown
         public Result OnShutdown(UIControlledApplication uiCtlApp)
         {
             return Result.Succeeded;
         }
+
+        #endregion
+
+        #region Use idling to register UiApp
+        // On idling, register UiApp/username
+        private static void RegisterUiApp(object sender, IdlingEventArgs e)
+        {
+            _uiCtlApp.Idling -= RegisterUiApp;
+
+            if (sender is UIApplication uiApp )
+            {
+                Globals.UiApp = uiApp;
+                Globals.UsernameRevit = uiApp.Application.Username;
+            }
+        }
+
+        #endregion
     }
 }
---
+++ General/Globals.cs
@@ -0,0 +1,66 @@
+﻿using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assembly = System.Reflection.Assembly;
+
+namespace RevitAddIn.General
+{
+    public static class Globals
+    {
+        #region Properties
+        // Applications
+        public static UIControlledApplication UiCtlApp { get; set; }
+        public static ControlledApplication CtlApp { get; set; }
+        public static UIApplication UiApp { get; set; }
+        
+        // Assembly
+        public static Assembly Assembly { get; set; }
+        public static string AssemblyPath { get; set; }
+
+        // Revit versioning
+        public static string RevitVersion { get; set;}
+        public static int RevitVersionInt { get; set; }
+                
+        // User names
+        public static string UsernameRevit { get; set; }
+        public static string UsernameWindows { get; set; }
+        
+        // Guids and versioning
+        public static string AddinVersionNumber { get; set; }
+        public static string AddinVersionName {get; set;}
+        public static string AddinName { get; set; }
+        public static string AddinGuid { get; set; }
+
+        #endregion
+
+        #region Register Method
+
+        // Method to register properties
+        public static void RegisterProperties(UIControlledApplication uiCtlApp)
+        {
+            UiCtlApp = uiCtlApp;
+            CtlApp = uiCtlApp.ControlledApplication;
+            // UiApp set on idling
+
+            Assembly = Assembly.GetExecutingAssembly();
+            AssemblyPath = Assembly.Location;
+
+            RevitVersion = CtlApp.VersionNumber;
+            RevitVersionInt = Int32.Parse(RevitVersion);
+
+            // Revit username set on idling
+            //UsernameRevit = UiApp.Application.Username;
+            UsernameWindows = Environment.UserName;
+
+            AddinVersionNumber = "26.03.01";
+            AddinVersionName = "WIP";
+            AddinName = "RevitAddIn";
+            AddinGuid = "F6E4C718-21A6-4BA3-9386-ED59A4340EF3";
+        }
+        #endregion
+    }
+}
